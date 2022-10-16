3dbed14ef6963c7e803934f9adac92152dffcf59
Basil Ossandon <bossandon@carelinx.com>
2022-10-16 03:35:19 -0300

    Added toggle menu

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,81 @@ import { Ground } from './components/Ground'
 import { Physics } from '@react-three/cannon'
 import { Cubes } from './components/Cubes'
 import { TextureSelector } from './components/TextureSelector'
+import { Popover } from '@headlessui/react'
 
+function MyPopover({ name, setName, setClientName }) {
+    return (
+        <Popover className="absolute mt-16 inset-0 ml-2  z-90">
+            <Popover.Button className="outline-none ">
+                <div className="shadow-2xl  flex justify-center text-gray-600 items-center  bg-white h-10 w-10 rounded">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-8 h-8"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                        />
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                    </svg>
+                </div>
+            </Popover.Button>
+
+            <Popover.Panel className="absolute z-10">
+                <div className="" onClick={(e) => e.stopPropagation()}>
+                    <div className="bg-white shadow-md rounded-lg">
+                        <div className="px-4 py-5 sm:p-6">
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">
+                                Update your name
+                            </h3>
+                            <form className="mt-5 sm:flex gap-2 flex flex-col sm:items-center">
+                                <div className="w-full sm:max-w-xs mr-2">
+                                    <label htmlFor="email" className="sr-only">
+                                        Email
+                                    </label>
+                                    <input
+                                        value={name}
+                                        // onMouseEnter={() => alert('entro!')}
+                                        // value={name}
+                                        onChange={(e) =>
+                                            setName(e.target.value)
+                                        }
+                                        type="text"
+                                        id="first_name"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500"
+                                        placeholder="John"
+                                        required
+                                    />
+                                </div>
+                                <div
+                                    onClick={(e) => {
+                                        setClientName(name)
+                                    }}
+                                    className="cursor-pointer text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
+                                >
+                                    Submit
+                                </div>
+                            </form>
+                            <br />
+                            Controls <br />
+                            WASD - Movement <br /> Shift - Run <br />
+                            1-5 - Select blocks
+                        </div>
+                    </div>
+                </div>
+            </Popover.Panel>
+        </Popover>
+    )
+}
 function ControlsWrapper({ clientName, socket }) {
     const { camera } = useThree()
     // Register the update event and clean up
@@ -69,50 +143,6 @@ function App() {
             <>
                 <div className="absolute inset-1/2 z-20 text-white">+</div>
 
-                <div
-                    className="absolute mt-14 ml-2 z-10"
-                    onClick={(e) => e.stopPropagation()}
-                >
-                    <div className="bg-white shadow-md sm:rounded-lg">
-                        <div className="px-4 py-5 sm:p-6">
-                            <h3 className="text-lg font-medium leading-6 text-gray-900">
-                                Update your name
-                            </h3>
-                            <form className="mt-5 sm:flex sm:items-center">
-                                <div className="w-full sm:max-w-xs mr-2">
-                                    <label htmlFor="email" className="sr-only">
-                                        Email
-                                    </label>
-                                    <input
-                                        value={name}
-                                        // onMouseEnter={() => alert('entro!')}
-                                        // value={name}
-                                        onChange={(e) =>
-                                            setName(e.target.value)
-                                        }
-                                        type="text"
-                                        id="first_name"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="John"
-                                        required
-                                    />
-                                </div>
-                                <div
-                                    onClick={(e) => {
-                                        setClientName(name)
-                                    }}
-                                    className="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                >
-                                    Submit
-                                </div>
-                            </form>
-                            <br />
-                            Controls <br />
-                            WASD - Movement <br /> Shift - Run <br />
-                            1-5 - Select blocks
-                        </div>
-                    </div>
-                </div>
                 <Canvas camera={{ position: [0, 1, 5] }}>
                     {/* <Sky } /> */}
                     {/* <ambientLight intensity={0.5} /> */}
@@ -165,6 +195,11 @@ function App() {
                         })}
                 </Canvas>
                 <TextureSelector />
+                <MyPopover
+                    name={name}
+                    setName={setName}
+                    setClientName={setClientName}
+                />
             </>
         )
     )
